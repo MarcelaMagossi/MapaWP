0e1cfa1fb4a70732d21da35f042f0d1c60cf9fa2
caio07 <caio.oliveira07@gmail.com>
2016-05-19 20:06:27 -0300

    Bootstrap funcionando
    Bootstrap funcionando com código originado do CDN

--- MapaEducacao/header.php
+++ MapaEducacao/header.php
@@ -7,7 +7,7 @@
   <link rel="icon" href="http://mapaeducacao.com/newproject/wp-content/uploads/2016/05/mapaicon.ico" sizes="16x16" type="image/png">
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,800,700|Oswald' rel='stylesheet' type='text/css'>
 
-  <link href="<?php bloginfo('template_url'); ?>bootstrap/css/bootstrap.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
