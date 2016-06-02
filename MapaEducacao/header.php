fc91a841e858e330350673ca72f499a01d5f1ed3
caio07 <caio.oliveira07@gmail.com>
2016-06-02 18:24:28 -0300

    Menu responsivo e fixo no topo
    Menu fixo no top e se tornando ícone ao atingir o tamanho mobile

--- MapaEducacao/header.php
+++ MapaEducacao/header.php
@@ -10,12 +10,7 @@
 
   <title><?php bloginfo('name'); ?></title>
 
-
-
   <link rel="icon" href="http://mapaeducacao.com/newproject/wp-content/uploads/2016/05/mapaicon.ico" sizes="16x16" type="image/png">
-
-
-
 
   <!--[if lt IE 9]>
 
@@ -23,45 +18,38 @@
 
     <![endif]-->
 
-
     <?php wp_head(); ?>
-
-
 
 </head>
 
-
-
 <body>
-
-
-
   <header>
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".navbar-collapse" role="button">
+              <span class="sr-only">Toggle Navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a href="#" class="navbar-brand"></a>
+          </div>
+          <?php
+              wp_nav_menu( array(
 
-   <?php
+                  'menu'              => 'primary',
+                  'theme_location'    => 'primary',
+                  'depth'             => 2,
+                  'container'         => 'div',
+                  'container_class'   => 'collapse navbar-collapse',
+                  'container_id'      => 'bs-example-navbar-collapse-1',
+                  'menu_class'        => 'nav navbar-nav',
+                  'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                  'walker'            => new wp_bootstrap_navwalker())
+              );
 
-            wp_nav_menu( array(
-
-                'menu'              => 'primary',
-
-                'theme_location'    => 'primary',
-
-                'depth'             => 2,
-
-                'container'         => 'div',
-
-                'container_class'   => 'collapse navbar-collapse',
-
-                'container_id'      => 'bs-example-navbar-collapse-1',
-
-                'menu_class'        => 'nav navbar-nav',
-
-                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-
-                'walker'            => new wp_bootstrap_navwalker())
-
-            );
-
-        ?>
-
+          ?>
+        </div>
+      </nav>
   </header>
